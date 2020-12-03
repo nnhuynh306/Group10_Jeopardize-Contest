2653d70a452eaebeebff2952832e7ace3a3f8b46
Nam Huynh Nhat <nnhuynh306@gmail.com>
2020-12-03 10:27:38 +0700

    install express-handlebars

--- main3.js
+++ main3.js
@@ -1,6 +1,16 @@
 const express = require('express');
-const hbs = require('handlebars');
+const { partials } = require('handlebars');
+const hbs = require('express-handlebars');
 var app = express();
+
+app.engine('hbs', hbs({
+    extname: 'hbs',
+    defaultLayout: 'layout',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+}))
+
+app.set('view engine', 'hbs');
 
 app.listen(5000, () => {
     console.log('Server is listening as port 5000...');
