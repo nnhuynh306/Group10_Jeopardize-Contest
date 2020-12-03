387faf49dc2dca88dc659d71a9adb4d85e5e3092
Nam Huynh Nhat <nnhuynh306@gmail.com>
2020-12-03 19:36:20 +0700

    clean up

--- main.js
+++ main.js
@@ -6,11 +6,21 @@ var app = express();
 
 app.use(express.static(__dirname))
 
+var ifCond = function(v1, v2, options) {
+  if(v1 === v2) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+};
+
 app.engine('hbs', hbs({
     extname: 'hbs',
     defaultLayout: 'layout',
     layoutsDir: __dirname + '/views/layouts',
     partialsDir: __dirname + '/views/partials',
+    helpers: {
+      ifCond: ifCond
+    }
 }));
 
 
@@ -21,85 +31,14 @@ app.get('/', function(req, res) {
 })
 
 app.get('/index.htm', function(req, res) {
-    res.render('index');
+  res.render('index');
 })
 
-app.get('/task3.htm', function(req, res) {
-    var data = {
-        categories: [
-            {
-              title: "Samsung", id: 1
-            }, 
-            {
-              title: "Sony", id: 2
-            }, 
-            {
-              title: "Panasonic", id: 3
-            }
-        ],
-        products: [
-            {
-              title: "TIVI QLED SAMSUNG QA55Q7FAMKXXV",
-              description: "Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
-              price: 57900000,
-              category: 1,
-              imagePath: "task3/smart-tv-4k-qled-q7f-01.jpg"
-            }, 
-            {
-              title: "INTERNET TIVI SONY 40 INCH KDL-40W650D",
-              description: "Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
-              price: 9900000,
-              category: 2, 
-              imagePath: "task3/tivi-lcd-sony-kdl-40w650d-01.jpg"
-            }, 
-            {
-              title: "TIVI LED PANASONIC 55 INCH TH-55DS630V", 
-              description: "Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.", 
-              price: 16990000, 
-              category: 3, 
-              imagePath: "task3/tivi-panasonic-th-55ds630v.jpg"
-            }, 
-            {
-              title: "TIVI LED PANASONIC 40 INCH TH-40DS500V", 
-              description: "Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.", 
-              price: 7990000, 
-              category: 3, 
-              imagePath: "task3/tivi-led-panasonic-th-40ds500v.jpg"
-            }, 
-            {
-              title: "ANDROID TIVI SONY 43 INCH KDL-43W800C", 
-              description: "Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.", 
-              price: 13400000, 
-              category: 2, 
-              imagePath: "task3/tivi-lcd-sony-kdl-43w800c-vn3.jpg"
-            }, 
-            {
-              title: "TIVI LED PANASONIC 58 INCH TH-58DX700V", 
-              description: "Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.", 
-              price: 28490000, 
-              category: 3, 
-              imagePath: "task3/tivi-led-panasonic-th-58dx700v.jpg"
-            }
-          ]
-    }
-    res.render('task3', data);
+app.get('/task1.htm', function(req, res) {
+  res.render('task1');
 })
-
-
-app.get('/task4.htm', function(req,res){
-  res.locals.list = data.zodiacs;
-  res.render('task4')
-})
-
-app.get('/task4-details.htm', function(req,res){
-  res.locals.zodiac = data.zodiacs[req.query.id];
-  res.render('task4-details');
-})
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
 
 app.post('/salary', (req, res) => {
   var task1_data = req.body
@@ -116,6 +55,148 @@ app.post('/salary', (req, res) => {
   res.render('task1', res_task1);
 });
 
+
+app.get('/task2', function(req, res) {
+  var data = {
+      emotions :[
+          {
+              title: 'happy',
+              imagePath: '/task2/happy.jpeg',
+              quotePath: '/task2/happy/happy1.jpg'
+          },
+          {
+              title: 'sad',
+              imagePath: '/task2/sad.jpeg',
+              quotePath: '/task2/sad/sad1.jpg'
+          },
+          {
+              title: 'stress',
+              imagePath: '/task2/stress.jpeg',
+              quotePath: '/task2/stress/stress1.jpg'
+          },
+          {
+              title: 'angry',
+              imagePath: '/task2/angry.jpeg',
+              quotePath: '/task2/angry/angry1.jpg'
+          }
+        ]
+      }
+  res.render('task2',{
+      emotions: data.emotions,
+      quotePath: '/task2/default.jpg'
+  })
+})
+
+app.get('/task2/:title', function(req, res) {
+  var data = {
+      emotions :[
+          {
+              title: 'happy',
+              imagePath: '/task2/happy.jpeg',
+              quotePath: '/task2/happy/happy1.jpg'
+          },
+          {
+              title: 'sad',
+              imagePath: '/task2/sad.jpeg',
+              quotePath: '/task2/sad/sad1.jpg'
+          },
+          {
+              title: 'stress',
+              imagePath: '/task2/stress.jpeg',
+              quotePath: '/task2/stress/stress1.jpg'
+          },
+          {
+              title: 'angry',
+              imagePath: '/task2/angry.jpeg',
+              quotePath: '/task2/angry/angry1.jpg'
+          }
+        ]
+      }
+  let quotePath = '';
+  for (let emotion of data.emotions){
+      if (emotion.title === req.params.title){
+          quotePath = emotion.quotePath;
+          break;
+      }
+  }
+  res.render('task2',{
+      emotions: data.emotions,
+      quotePath: quotePath
+  })
+})
+
+
+app.get('/task3', function(req, res) {
+  var data = {
+    categories: [
+        {
+          title: "Samsung", id: 1
+        }, 
+        {
+          title: "Sony", id: 2
+        }, 
+        {
+          title: "Panasonic", id: 3
+        }
+    ],
+    products: [
+        {
+          title: "TIVI QLED SAMSUNG QA55Q7FAMKXXV",
+          description: "Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
+          price: 57900000,
+          category: 1,
+          imagePath: "task3/smart-tv-4k-qled-q7f-01.jpg"
+        }, 
+        {
+          title: "INTERNET TIVI SONY 40 INCH KDL-40W650D",
+          description: "Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
+          price: 9900000,
+          category: 2, 
+          imagePath: "task3/tivi-lcd-sony-kdl-40w650d-01.jpg"
+        }, 
+        {
+          title: "TIVI LED PANASONIC 55 INCH TH-55DS630V", 
+          description: "Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.", 
+          price: 16990000, 
+          category: 3, 
+          imagePath: "task3/tivi-panasonic-th-55ds630v.jpg"
+        }, 
+        {
+          title: "TIVI LED PANASONIC 40 INCH TH-40DS500V", 
+          description: "Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.", 
+          price: 7990000, 
+          category: 3, 
+          imagePath: "task3/tivi-led-panasonic-th-40ds500v.jpg"
+        }, 
+        {
+          title: "ANDROID TIVI SONY 43 INCH KDL-43W800C", 
+          description: "Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.", 
+          price: 13400000, 
+          category: 2, 
+          imagePath: "task3/tivi-lcd-sony-kdl-43w800c-vn3.jpg"
+        }, 
+        {
+          title: "TIVI LED PANASONIC 58 INCH TH-58DX700V", 
+          description: "Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.", 
+          price: 28490000, 
+          category: 3, 
+          imagePath: "task3/tivi-led-panasonic-th-58dx700v.jpg"
+        }
+      ]
+}
+    res.render('task3', data);
+})
+
+
+app.get('/task4.htm', function(req,res){
+  res.locals.list = data.zodiacs;
+  res.render('task4')
+})
+
+app.get('/task4-details.htm', function(req,res){
+  res.locals.zodiac = data.zodiacs[req.query.id];
+  res.render('task4-details');
+})
 
 app.listen((process.env.PORT || 5000), () => {
 
--- scripts/tabs.js
+++ scripts/tabs.js
@@ -1,23 +1,20 @@
 function openLabel(evt, labelName) {
-    console.log(labelName)
+
     // Declare all variables
     var i, tabcontent, tablinks;
   
-    // Get all elements with class="tabcontent" and hide them
+    // remove all active class
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+      tabcontent[i].className = tabcontent[i].className.replace(" active", "");
     }
   
-    // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
-    console.log(labelName)
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(labelName).style.display = "block";
-    console.log(document.getElementById(labelName))
+    // add active class to show current tab
+    document.getElementById(labelName).classList.add("active");
     evt.currentTarget.className += " active";
   }
