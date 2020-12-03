856490cd83df94818df9331ac15b631a9b9f1d3a
Nam Huynh Nhat <nnhuynh306@gmail.com>
2020-12-03 10:21:41 +0700

    create folders for handlebars

---
+++ main3.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const hbs = require('handlebars');
+var app = express();
+
+app.listen(5000, () => {
+    console.log('Server is listening as port 5000...');
+})
